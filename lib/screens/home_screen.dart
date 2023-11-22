521c7a90827ed5f5baaadca5bbfe809f88712698
Florin V <florin@yourcamp.us>
2023-11-22 16:36:48 +0200

    body completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,18 +12,109 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Human body screen 2")),
-      body: SafeArea(
-        child: Stack(children: [
-          Center(
-            child: Column(
-              children: [
-                Image.asset("assets/head1.png"),
-                Image.asset("assets/neck.png"),
-                Image.asset("assets/shoulders_and_chest.png"),
-              ],
-            ),
-          )
-        ]),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Stack(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 0, top: 0),
+                    child: Image.asset(
+                      "assets/head_and_neck.png",
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("head tapped");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: Container(
+                        width: 100,
+                        height: 120,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              Center(
+                child: Stack(children: [
+                  Image.asset(
+                    "assets/abs_and_chest.png",
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("head tapped");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0, bottom: 10),
+                      child: Container(
+                        width: 10,
+                        height: 120,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              Center(
+                child: Stack(children: [
+                  Image.asset(
+                    "assets/arms_and_pelvis.png",
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("head tapped");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0, bottom: 10),
+                      child: Container(
+                        width: 50,
+                        height: 120,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("head tapped");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 28.0, bottom: 40),
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              Center(
+                child: Stack(children: [
+                  Image.asset(
+                    "assets/quads_knees_calves_and_feet.png",
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("head tapped");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Container(
+                        width: 10,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
